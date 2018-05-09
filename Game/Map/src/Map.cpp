522e2f1a26b8af3a59464ac2e859cf9055c8f7dc
marie-aimee.fourtane@epitech.eu <marie-aimee.fourtane@epitech.eu>
2018-05-09 15:18:11 +0200

    début du moove et du mapTranslator

---
+++ Game/Map/src/Map.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2018
+** indie
+** File description:
+** source
+*/
+
+#include "Map.hpp"
+
+Map::Map()
+{
+}
+
+Map::~Map()
+{
+}
---
+++ Game/Map/src/Map.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2018
+** indie
+** File description:
+** header
+*/
+
+#ifndef MAP_HPP_
+	#define MAP_HPP_
+
+class Map {
+	public:
+		Map();
+		~Map();
+
+	protected:
+	private:
+};
+
+#endif /* !MAP_HPP_ */
