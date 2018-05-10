727c9d6e1c0757345f1d4cb452f397e724507b9c
Unknown <m.cescutti@hotmail.fr>
2018-05-10 17:22:29 +0200

    ajout main de tst irrlicht

--- Sources/Core/src/main.cpp
+++ Sources/Core/src/main.cpp
@@ -5,10 +5,23 @@
 ** source
 */
 
+#include <irrlicht/irrlicht.h>
+#include "IrrlichtLib.hpp"
 #include <iostream>
 
 int main()
 {
 	std::cout << "Hello Indie !!" << std::endl;
+
+	int main()
+	{
+		graphic::IrrlichtLib Lib;
+		Lib.setMesh();
+		while(Lib._device->run())
+		{
+		        Lib._guiEnv->addStaticText(L"WRITE SOMETHING", irr::core::rect<irr::s32>(10,10,260,22), true);
+		        Lib.displayAll();
+		}
+	}
 	return 0;
 }
