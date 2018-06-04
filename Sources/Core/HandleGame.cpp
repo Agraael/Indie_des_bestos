7a818db9535942b6f8a0bf8e6305f3e0acabba12
kiwiv2 <alexandre.caila@epitech.eu>
2018-06-04 11:40:56 +0200

    add texture

--- Sources/Core/HandleGame.cpp
+++ Sources/Core/HandleGame.cpp
@@ -19,15 +19,15 @@ void    HandleGame::InitGame()
 	generator->runGeneration(GenerationSize::Big, GenerationMod::FullDest);
 	_threeDMap = std::make_shared<Map>(Map(interpret->createMap(generator->getMap())));
 	map = generator->getMap();
-	//for (int y = 0; map[y] != NULL; y++) {
-	//	for (int x = 0; map[y][x] != '\0'; x++) {
-	//		vec3df	vec;
-	//		vec.x = static_cast<double>(x);
-	//		vec.y = static_cast<double>(y);
-	//		vec.z = 0;
-	//		_lib->createCube(vec, "./Assets/media/pixel_skyline.png");
-	//	}
-	//}
+	for (int y = 0; map[y] != NULL; y++) {
+		for (int x = 0; map[y][x] != '\0'; x++) {
+			vec3df	vec;
+			vec.x = static_cast<double>(x);
+			vec.y = static_cast<double>(y);
+			vec.z = 0;
+			_lib->createCube(vec, "./Assets/media/brick_wall.jpg");
+		}
+	}
 	vec3df	vec;
 	vec.x = 10;
 	vec.y = -10;
