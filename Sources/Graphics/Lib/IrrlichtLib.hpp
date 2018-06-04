0860ac0f912aca40176bdea2f06b2283c5633802
kiwiv2 <alexandre.caila@epitech.eu>
2018-06-04 11:36:21 +0200

    core aff game

--- Sources/Graphics/Lib/IrrlichtLib.hpp
+++ Sources/Graphics/Lib/IrrlichtLib.hpp
@@ -7,43 +7,51 @@
 
 #include <iostream>
 #include <map>
+#include <string>
 #include <irrlicht/irrlicht.h>
 #include "LibEventManager.hpp"
 
+struct vec3df {
+	double	x;
+	double	y;
+	double	z;
+};
+
 namespace graphic {
-    typedef struct infos_s
-    {
-        size_t _x;
-        size_t _y;
-        size_t _w;
-        size_t _h;
-        std::string _path;
-        std::string _name;
-        std::string _desc;
-        graphic::controllerUser _type;
-    } infos_t;
-    const wchar_t *convertStringToWString(const std::string &);
-    class IrrlichtLib {
-    public:
-        IrrlichtLib();
-        ~IrrlichtLib();
-        irr::video::ITexture *findTextureOrCreate(const std::string &path);
-        irr::gui::IGUIImage *drawImage(int posx, int posy, int w, int h, const std::string &path);
-        irr::IrrlichtDevice *getDevice() { return _device; }
-        void displayAll();
-        irr::scene::ISceneNode *createCube();
-        irr::scene::ISceneNode *createSphere();
-        void setCamera(irr::scene::ISceneNode *parent);
-        void drawEditBox(graphic::infos_t infos);
-        irr::gui::IGUIButton *printButton(const infos_t &infos);
-        void drawText(size_t x, size_t y, size_t fontSize, std::string const&);
-    private:
-        irr::video::IVideoDriver* _driver;
-        irr::scene::ISceneManager* _managerScene;
-        irr::IrrlichtDevice *_device;
-        irr::gui::IGUIEnvironment* _guiEnv;
-        std::map<std::string, irr::video::ITexture *> _mapTexture;
-    };
-    void driverChoiceConsole(irr::video::E_DRIVER_TYPE &);
+	typedef struct infos_s
+	{
+		size_t _x;
+		size_t _y;
+		size_t _w;
+		size_t _h;
+		std::string _path;
+		std::string _name;
+		std::string _desc;
+		graphic::controllerUser _type;
+	} infos_t;
+	const wchar_t *convertStringToWString(const std::string &);
+	class IrrlichtLib {
+	public:
+		IrrlichtLib();
+		~IrrlichtLib();
+		irr::video::ITexture *findTextureOrCreate(const std::string &path);
+		irr::gui::IGUIImage *drawImage(int posx, int posy, int w, int h, const std::string &path);
+		irr::IrrlichtDevice *getDevice() { return _device; }
+		void displayAll();
+		irr::scene::ISceneNode *createCube(const vec3df &, const std::string &);
+		irr::scene::ISceneNode *createSphere();
+		void setCamera(irr::scene::ISceneNode *parent);
+		void setCamera(const vec3df &pos, const vec3df &target);
+		void drawEditBox(graphic::infos_t infos);
+		irr::gui::IGUIButton *printButton(const infos_t &infos);
+		void drawText(size_t x, size_t y, size_t fontSize, std::string const&);
+	private:
+		irr::video::IVideoDriver* _driver;
+		irr::scene::ISceneManager* _managerScene;
+		irr::IrrlichtDevice *_device;
+		irr::gui::IGUIEnvironment* _guiEnv;
+		std::map<std::string, irr::video::ITexture *> _mapTexture;
+	};
+	void driverChoiceConsole(irr::video::E_DRIVER_TYPE &);
 }
 #endif //IRRLICHTLIB_HPP
