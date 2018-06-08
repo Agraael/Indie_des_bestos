ce9c58b3bcd154c15862246a48152f2e55ddd9c8
Unknown <m.cescutti@hotmail.fr>
2018-06-08 17:46:19 +0200

    test installation irrklang , pending for test

--- Sources/Core/main.cpp
+++ Sources/Core/main.cpp
@@ -5,22 +5,51 @@
 ** main
 */
 
+//#include <iostream>
+//#include <exception>
+//#include "Core.hpp"
+//
+//int	main(int ac, char **av)
+//{
+//	Core	core;
+//
+//	(void)ac;
+//	(void)av;
+//	try
+//	{
+//	if (core.run() == 84)
+//		return (84);
+//	} catch(std::exception ex) {
+//		std::cout << ex.what() << std::endl;
+//	}
+//	return (0);
+//}
+
+
+#include <memory>
+#include "../LocalMenu/localMenu.hpp"
+#include <irrKlang.h>
+#include <string>
+#include <cstdio>
 #include <iostream>
-#include <exception>
-#include "Core.hpp"
+#include "irrKlang.h"
 
-int	main(int ac, char **av)
+using namespace std;
+
+int main()
 {
-	Core	core;
 
-	(void)ac;
-	(void)av;
-	try
-	{
-	if (core.run() == 84)
-		return (84);
-	} catch(std::exception ex) {
-		std::cout << ex.what() << std::endl;
-	}
-	return (0);
+	irrklang::ISoundEngine* engine = irrklang::createIrrKlangDevice();
+
+	if (!engine) return 1; // could not start engine
+
+	engine->setSoundVolume(1.0f);
+	engine->play2D("./media/bombermanMenu.mp3"); // play some mp3 file
+
+	cout<<"hello\n";
+
+	std::cin.get(); // wait until user presses a key
+
+	engine->drop(); // delete engine
+	return 0;
 }
