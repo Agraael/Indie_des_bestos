27153f2da63e88d937e967d5b19a2b5f4f303267
marie-aimee.fourtane@epitech.eu <marie-aimee.fourtane@epitech.eu>
2018-06-09 16:21:00 +0200

    liste addedEntities for alexandre

--- Sources/Game/Map/Map.hpp
+++ Sources/Game/Map/Map.hpp
@@ -15,29 +15,34 @@
 
 class Map {
 public:
-	Map(GameMap map) : _map(map) {};
+	Map(GameMap map) :_map(map) { _mapPos = std::make_pair(map.size(), map[0].size()); }
 	GameMap &get3dMap() { return _map; }
-	void	placeBomb(entities::entityPosition, std::size_t);
 	void	updatePos(entities::Entity *, entities::entityPosition);
 	void	displayMap();
 	void	checkAfterExplosion() {};
 	void	playerDeath() {};
-	std::vector<std::shared_ptr<entities::Entity>> getModifiedEntities(void) const noexcept { return _modifiedEntities; }
+	void	placeBomb(entities::entityPosition, std::size_t);
 	void    checkBonusCollision(std::shared_ptr<entities::Entity>, const entities::entityPosition &);
 	void	checkExplosionCollision(const entities::entityPosition &_pos);
-	std::vector<std::size_t>	getDeleteEntities() const noexcept { return _deletedEntities; }
+	std::vector<std::shared_ptr<entities::Entity>>	getModifiedEntities(void) const noexcept { return _modifiedEntities; }
+	std::vector<std::size_t>			getDeleteEntities() const noexcept { return _deletedEntities; }
+	std::vector<std::shared_ptr<entities::Entity>>	getAddedEntity() const noexcept { return _addedEntities; }
 	void	clearDeleteEntities() { _deletedEntities.clear(); }
+	bool	verifPosition(entities::entityPosition &);
 private:
 	void	placeExplosion(std::vector<std::shared_ptr<entities::Entity>> &, std::shared_ptr<entities::Entity> &, entities::entityPosition pos);	
 	void	addModifiedEntity(const std::shared_ptr<entities::Entity> &entity);
 	void	addDeletedEntity(const std::shared_ptr<entities::Entity> &entity);
+	void	addAddedEntity(const std::shared_ptr<entities::Entity> &entity);
 	void    addBombs(std::shared_ptr<entities::Entity> &character, const entities::entityPosition &pos);
         void    addSpeed(std::shared_ptr<entities::Entity> &character, const entities::entityPosition &pos);
         void    addFire(std::shared_ptr<entities::Entity> &character, const entities::entityPosition &pos);
         void    allowWallpass(std::shared_ptr<entities::Entity> &character, const entities::entityPosition &pos);
 	std::vector<std::shared_ptr<entities::Entity>> _modifiedEntities;
+	std::vector<std::shared_ptr<entities::Entity>> _addedEntities;
 	std::vector<std::size_t> _deletedEntities;
 	GameMap _map;
+	std::pair<int, int>	_mapPos;
 };
 
 #endif /* !MAP_HPP_ */
